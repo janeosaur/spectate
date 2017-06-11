a417d77e3c94adebc56995716d6ac4e6f8c402bd
janeosaur <janewie@gmail.com>
2017-06-11 15:19:18 -0700

    reorganized user profile page forms

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,9 @@
 class User < ApplicationRecord
   has_many :events
-  validates :photo, allow_blank: true, format: { with: %r{.(gif|jpg|png)\Z}i, message: 'must be a URL for GIF, JPG or PNG image.' }
-  validates :cover_photo, allow_blank: true, format: { with: %r{.(gif|jpg|png)\Z}i, message: 'must be a URL for GIF, JPG or PNG image.' }
+  validates :photo, allow_blank: true, format: { with: %r{.(gif|jpg|png)\Z}i, message: ': must be a URL for GIF, JPG or PNG image.' }
+  validates :cover_photo, allow_blank: true, format: { with: %r{.(gif|jpg|png)\Z}i, message: ': must be a URL for GIF, JPG or PNG image.' }
+  # validates :twitter, allow_blank: true, format: { with: #enter regexp here, message: ': just the username please'}
+  # validates :instagram, allow_blank: true,
 
 
   extend FriendlyId
