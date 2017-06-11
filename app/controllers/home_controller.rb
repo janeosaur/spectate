1a8e4df765139528ce583a904f9e9ee4dac9a66a
janeosaur <janewie@gmail.com>
2017-06-10 22:05:34 -0700

    event partial shows specified month

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,7 +6,8 @@ class HomeController < ApplicationController
   # get "/fetch_month", to: "events#by_month", as: "fetch_month"
   def by_month
     p params[:month]
-    @selected = Event.where('extract(month from date) = ?', 07)
+    @selected = Event.where('extract(month from date) = ?', params[:month].to_i)
+    p @selected
     respond_to do |format|
       format.js
     end
