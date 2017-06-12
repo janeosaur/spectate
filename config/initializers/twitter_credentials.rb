9c3a6a8d4741ef93a9391d726bb181b29dc13701
janeosaur <janewie@gmail.com>
2017-06-12 12:06:31 -0700

    twitter gem installed and data is accessible

--- config/initializers/twitter_credentials.rb
+++ config/initializers/twitter_credentials.rb
@@ -1,8 +1,8 @@
 require 'twitter'
 
-Twitter.configure do |config|
+$client = Twitter::REST::Client.new do |config|
   config.consumer_key = 'uLmBusLAJEhEXH4ZRoGxfI7Nv'
   config.consumer_secret = 'nJ4IkkS6g9ELZ2MRENVOxYbB97aLRy3WoD7j56voodJiUQUtjS'
-  config.oauth_token = '2155869889-kELlluhSislgjBxQaWePiAcAoYy7N3NgZxxv5M4'
-  config.oauth_token_secret = 'vx90zrrOJboTvUQkC857fXTBNXXY67JQ2scdPRrcXFSrC'
+  config.access_token = '2155869889-kELlluhSislgjBxQaWePiAcAoYy7N3NgZxxv5M4'
+  config.access_token_secret = 'vx90zrrOJboTvUQkC857fXTBNXXY67JQ2scdPRrcXFSrC'
 end
