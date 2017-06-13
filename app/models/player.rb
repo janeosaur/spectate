cb2231d0d362c695466ea3a0d3ee0e1d35b2a608
janeosaur <janewie@gmail.com>
2017-06-12 21:05:43 -0700

    friendly ID set up for players

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,3 +1,7 @@
 class Player < ApplicationRecord
   belongs_to :owner, optional: true
+
+  extend FriendlyId
+  friendly_id :gamer_tag, use: [:slugged]
+  
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170613035242) do
+ActiveRecord::Schema.define(version: 20170613040408) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -88,6 +88,8 @@ ActiveRecord::Schema.define(version: 20170613035242) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "gamer_tag"
+    t.string "slug"
+    t.index ["slug"], name: "index_players_on_slug", unique: true
     t.index ["team_id"], name: "index_players_on_team_id"
   end
 
